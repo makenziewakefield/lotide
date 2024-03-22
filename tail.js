b926a07afe1a763c2da2e84c854fb2ee6b335307
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-03-22 13:21:20 -0600

    Created a function that returns the tail of an array as a new array

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr += arr[i];
+  }
+  return newArr;
+};
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
