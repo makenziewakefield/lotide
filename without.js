2b26a8ce88c307a77ebf03c2701df79485c9ecf8
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-03-23 17:39:53 -0600

    Created a funtion that accepts two arrays and returns a new array that only includes the elements that were not present in the second array.

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (a1, a2) {
+  let value = eqArrays(a1, a2);
+  if (value) {
+    console.log(`✅✅✅Assertion Passed: ${a1} === ${a2}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${a1} !=== ${a2}`);
+  }
+};
+
+
+const without = function (source, itemsToRemove) {
+  return source.filter(i => !itemsToRemove.includes(i));
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without([], []));
+console.log(without([1], [4, 3, 2]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
