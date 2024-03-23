5373e9676dbfc14da61ff436726c385dc1bbaaf1
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-03-23 15:53:21 -0600

    Made a syntax change

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(a1, a2) {
   let value = eqArrays(a1, a2);
-  if (value === true) {
+  if (value) {
     console.log(`✅✅✅Assertion Passed: ${a1} === ${a2}`);
   } else {
     console.log(`❌❌❌Assertion Failed: ${a1} !=== ${a2}`);
