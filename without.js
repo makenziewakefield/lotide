a2b8005b031c3530dd95b3f5e0631b8d0221de46
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-03-23 20:03:21 -0600

    Uncommented test cases

--- without.js
+++ without.js
@@ -40,6 +40,6 @@ console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); 
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
