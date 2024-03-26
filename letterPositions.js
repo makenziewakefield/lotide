c4d1d10b713a060a6e0d6753f945ef9cf55b7827
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-03-26 15:20:40 -0600

    Created a function that accepts a string and returns an object with all the indices in which each character of the string is found.

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(a1, a2) {
+  let value = eqArrays(a1, a2);
+  if (value) {
+    console.log(`✅✅✅Assertion Passed: ${a1} === ${a2}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${a1} !== ${a2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char !== ' ') {
+      if (results[char]) {
+        results[char].push(i);
+      } else {
+        results[char] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
