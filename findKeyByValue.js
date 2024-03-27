e8fb2f878e6a60250b85d4ed639e39cfa4d63888
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-03-27 14:32:35 -0600

    Created a function that takes an object and a string value and looks through the object and returns the first key which contains the given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (object, value) {
+  for (const key in object) {      // loops over the object, key represents each property inside the object.
+    if (object[key] === value) {   // checks if object key (bestTVShowsByGenre[comedy]) is equal to the value that was passed into the function.
+      return key;                  // if the last line is true the function returns the key of the value that was passed.
+    }
+  }
+  return undefined                 // if there was no match found the function returns undefined.
+}
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
