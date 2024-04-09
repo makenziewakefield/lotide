a7d4f8b6300598f0c4ef7b7629eb94df0a1fd26b
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-09 12:11:18 -0600

    working on comparing key values that are arrays inside the function. Still in progress.

--- eqObjects.js
+++ eqObjects.js
@@ -29,6 +29,11 @@ const eqObjects = function(object1, object2) {
     if (!(key in object2) || object1[key] !== object2[key]) {
       return false;
     }
+    if (!Array.isArray(object1[key])) {
+      if (!Array.isArray(object2[key]) || !eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }
   }
   return true;
 };
