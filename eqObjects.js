a2f8d16f9c354da18bb9d21db617a3fa3e42cbe8
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-09 11:53:22 -0600

    Created a eqObjects function that accepts two objects and compares them and returns true or false.

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,15 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -6,12 +18,27 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-
 const eqObjects = function(object1, object2) {
-  
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (!(key in object2) || !eqArrays(object1[key], object2[key])) {
+      return false;
+    }
+  }
+  for (let key in object2) {
+    if (!(key in object1) || !eqArrays(object2[key], object1[key])) {
+      return false;
+    }
+  }
+  return true;
 };
+
+
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
@@ -19,3 +46,14 @@ assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
 
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
