619bc40883eeb3a77c768f9456de71a922706881
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 16:31:15 -0600

    incorporated mocha and chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
+const assert = require('chai').assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+describe("#middle", () => {
+
+  it("returns an empty array when given an array of two elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns an empty array when given and array of one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns the middle element of an array", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns the two middle elements of an array if the array has an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+
+});
