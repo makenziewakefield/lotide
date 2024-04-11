31099971ed1a82f98a245c67369511fc66dd4532
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 14:09:44 -0600

    new file for assertArraysEqual.js test code

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const eqArrays = require("../eqArrays");
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([4, 5, 6], [4, 5, 6]);
+assertArraysEqual([], []);
