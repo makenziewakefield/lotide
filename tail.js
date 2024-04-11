1a4d695cfe0a1c53fa24d21134c935e791247b31
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 16:22:22 -0600

    incorporated mocha and chai testing

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let newArr = [];
   for (let i = 1; i < arr.length; i++) {
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,24 @@
+const assert = require('chai').assert;
 const tail = require("../tail")
-const assertEqual = require("../assertEqual")
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs")
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+
+describe("#tail", () => {
+
+  it("returns an empty array when given an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+  it("returns an array without the first element", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+});
