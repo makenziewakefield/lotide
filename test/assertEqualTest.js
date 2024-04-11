44ab15dabaaed06b16611d8ec8169b11da16375c
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 13:22:54 -0600

    made a file to hold the test code from the assertEqual.js file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual(72, 72);
+assertEqual(38, 38);
