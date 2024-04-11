b675ce54b061d3052c8f23375d379391f8b5e388
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 13:54:20 -0600

    importing and exporting function for test directory

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,12 +11,4 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-// console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [4, 5, 6]), false);
+module.exports = eqArrays;
