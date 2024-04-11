910c89f1ba895c3e5425669697dff08b356458df
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 13:20:13 -0600

    importing and exporting function

--- assertEqual.js
+++ assertEqual.js
@@ -6,11 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(72, 72);
-assertEqual(38, 38);
-
 
 module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(array) {
   return array[0];
@@ -12,4 +6,4 @@ const head = function(array) {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head[8], 8);
+assertEqual(head([8]), 8);
