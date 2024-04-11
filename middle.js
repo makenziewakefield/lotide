6aadbae733eb66a210594c8bd05b75939dcbb9a0
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-11 14:25:02 -0600

    importing and exporting functions for test directory

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(a1, a2) {
-  let value = eqArrays(a1, a2);
-  if (value) {
-    console.log(`✅✅✅Assertion Passed: ${a1} === ${a2}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${a1} !=== ${a2}`);
-  }
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(arr) {
   let length = arr.length;
@@ -33,12 +14,5 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
